b4c99ac8ef8a72ea0412295f92ab0e1d15ca0d35
JohnOmena <jvfo@ic.ufal.br>
2020-08-17 21:40:45 -0300

    Adicionando conexao com banco de dados

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hi John' }));
+routes.get('/', async (req, res, next) => {
+  const user = await User.create({
+    name: 'John Victor',
+    email: 'jvfo@ic.ufal.br',
+    password_hash: '1578541564',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
